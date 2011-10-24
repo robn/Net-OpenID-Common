3865a63846926a9bf22a4b50863691ae84d02138
Roger Crew <crew@cs.stanford.edu>
2011-10-24 05:41:35 -0700

    Do not do _extract_head_markup_only on YADIS documents
    even in the case where we're retrieving HTML pages,
    we're only looking at the headers anyway.

--- lib/Net/OpenID/Yadis.pm
+++ lib/Net/OpenID/Yadis.pm
@@ -109,7 +109,11 @@ sub _get_contents {
     my $self = shift;
     my  ($url, $final_url_ref, $content_ref, $headers_ref) = @_;
 
-    my $res = Net::OpenID::URIFetch->fetch($url, $self->consumer, \&OpenID::util::_extract_head_markup_only);
+    # we do NOT do <body> elimination here because
+    # if it's an HTML document, we are only ever looking at the headers, and
+    # if it's a YADIS document, <body> elimination is not appropriate
+    # (YADIS is not HTML; film at 11)
+    my $res = Net::OpenID::URIFetch->fetch($url, $self->consumer, sub {  } );
 
     if ($res) {
         $$final_url_ref = $res->final_uri;
